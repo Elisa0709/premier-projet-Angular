0b43ce286d2d0cedf43b8a7c96eb89b5dc2acd31
ElisaLeroy <elisa.leroy@Digital-Grenoble.local>
2025-01-31 15:47:20 +0100

    leçon réagir aux evenements - terminée

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
   templateUrl: './face-snap.component.html',
   styleUrl: './face-snap.component.scss'
 })
-export class FaceSnapComponent {
+
+export class FaceSnapComponent implements OnInit{
+  title!: string;
+  description!: string;
+  createdAt!: Date;
+  isSnapped!: boolean;
+  snaps!: number;
+  imageUrl!: string;
+
+  ngOnInit(): void {
+    this.title = 'Elisa';
+    this.description = 'J\'apprend Angular !';
+    this.createdAt = new Date();
+    this.snaps = 5;
+    this.isSnapped = false;
+    this.imageUrl = 'https://cdn.pixabay.com/photo/2024/05/20/13/28/ai-generated-8775234_1280.png';
+  }
+
+  onAddSnap(): void {
+    if (this.isSnapped == true) {
+      this.snaps--;
+      this.isSnapped = false;
+
+    }else{
+      this.snaps++;
+      this.isSnapped = true;
+    }
+  }
+
+
 
 }
