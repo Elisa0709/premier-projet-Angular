d1a1bf849f6268a0e0c8f70672e15179bdc08939
Elisa LEROY <elisa.leroy@Digital-Grenoble.local>
2025-01-31 14:23:11 +0100

    ajout du premier component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FaceSnapComponent} from './face-snap/face-snap.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FaceSnapComponent
+  ],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'firstAngularProject';
 }
